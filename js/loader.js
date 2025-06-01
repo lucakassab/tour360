f77840c6c97760df3c336c48e3484c316135e267
lucakassab <luca.kassab.c@gmail.com>
2025-06-01 17:02:28 -0300

    Deploy autom├ítico (Sun 06/01/2025 17:02:28.17)

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 (async ()=>{
   const isMobile=/Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
   const canVR=navigator.xr && await navigator.xr.isSessionSupported?.('immersive-vr');
-  const entry= canVR ? './js/vr.js' : (isMobile ? './js/mobile.js' : './js/desktop.js');
+  const entry= canVR ? './vr.js' : (isMobile ? './mobile.js' : './desktop.js');
   import(entry).catch(e=>console.error('Falhou ao carregar',entry,e));
 })();
